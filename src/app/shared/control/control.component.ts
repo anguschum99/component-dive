6c06568ce75f12184209ea6fe79cc3e25c8d805b
Angus Chum <52922624+anguschum99@users.noreply.github.com>
2025-09-25 15:44:30 -0400

    Refactor dashboard item and button components for improved structure and styling

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,13 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, HostBinding, HostListener, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   standalone: true,
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'control',
+    '(click)': 'onClick()'
+  }
 })
 export class ControlComponent {
-  @Input({required: true}) label!: string;
+  // @HostBinding('class') className = 'control'; Don't use this
+  @Input({ required: true }) label!: string;
   @Input() subtitle: string = '';
+
+  // @HostListener('click') onClick() {
+  //   console.log('clickeddd');
+  // }
+
+  onClick(){
+    console.log('clicked');
+  }
+
+
 }
